04ccf5743ad0c39da85cfdfdeef3eae0955a92ce
Kris Hvattum <klhvattum@live.com>
2024-01-16 22:58:10 -0800

    style home page, add footer w/o content

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../components/header/Header';
+import Footer from '@/components/footer/Footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
             <body className={inter.className}>
                 <Header />
                 {children}
+                <Footer />
             </body>
         </html>
     )
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,29 +1,42 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './page.module.scss';
-import { Button } from 'react-bootstrap';
 
 export default function Home() {
     return (
         <main className={styles.main}>
             <div className={styles.heroBg}>
                 <div className={styles.heroTitleContainer}>
-                    <h1 className={styles.heroTitle}>88.5 KRLF</h1>
+                    <h1 className={styles.heroTitle}><em>Alive</em> 88.5 KRLF</h1>
                     <h2 className={styles.heroTitle}>Christian Radio for the Palouse Region</h2>
-                    <Link className={styles.prayerButton} href='/contact/prayer-requests'>Prayer Requests</Link>
                 </div>
             </div>
             <div className={styles.homeTextContainer}>
                 <article>
-                    <h3><em>As a Christian radio station, we believe that God answers prayer</em></h3>
+                    <section className={`${styles.homeText} ${styles.homeText1}`}>
+                        <Image
+                            src='/worship.jpg'
+                            width='2222'
+                            height='1472'
+                            alt='hands raised with the word Jesus in the background'
+                            className={styles.homeImg}
+                        />          
+                        <div>
+                        <h3><em>You will hear music from artists such as...</em></h3>
+
+                        <p> TobyMac, Mercy Me, NeedToBreathe, Laura Story, Natalie Grant, Chris August, Third Day, Newsboys, Kutless, Casting Crowns, Fireflight, Tenth Avenue North, Sanctus Real, Chris Tomlin, Brandon Heath.</p>                            
+                        </div>              
+
+                    </section>
+                    <section className={styles.homeImgSection}>
+
+                    </section>
+
                     <section className={styles.homeText}>
-                        <p>During every KRLF small group we have a specified "Prayer Time" when we pray for each other, KRLF and its listeners, and the greater Palouse region. </p>
+                        <p>We know that there is power in music and that the songs that stay with you and play over and over again in your head should be songs that give you hope and build your faith.  AND we try to provide music and programming that would appeal to the entire family.</p>
                     </section>
                     <section className={styles.homeText}>
-                        <p>If you would like to submit a prayer request to the group please email us at: krlf@krlf.org with "KRLF Small Group Prayer Request" in the subject line.</p>
-                    </section>
-                    <section className={styles.homeText}>
-                        <p>We will add any requests to our prayers at the next KRLF small group meeting.</p>
+                        <p>We are a local station run mostly by volunteers.  We do quite a bit of our own programming (check out our Programs page!), but the bulk of our music feed and live DJ talk comes from a growing Christian Adult Contemporary music network, Christian FM.  We chose Christian FM because they, like us, are "on a mission".  Our mission is to spread the good news of Jesus Christ and to help the Christians in our area grow in their spiritual walk through our choice of music and programming.  We hope that we are doing just that and we hope that you, our listeners, like the changes that we have made.  We want you to feel like this is your station and we always welcome your feedback.</p>
                     </section>
                 </article>
             </div>
