923daacc357ee38000d677180ea884bf7801b806
Kris Hvattum <klhvattum@live.com>
2024-01-17 17:48:40 -0800

    add content to footer, style header

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,20 @@
-import './footer.scss'
-
-import React from 'react'
+import './footer.scss';
+import Link from 'next/link';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer>
+            <div className='copyright-div'>
+                <p>&#169;2023 Living Faith Fellowship</p>
+                <address>Pullman, WA</address>
+            </div>
+            <div className='email-div'>
+                <p>Questions or comments:</p>
+                <Link href='mailto:krlf@krlf.org'>krlf@krlf.org</Link>
+            </div>
+        </footer>
+
+    )
 }
 
 export default Footer
